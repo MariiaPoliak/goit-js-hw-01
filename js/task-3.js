36f0468d43caae1290fa7dca6d0f35d99c44060a
Mariia <mariiapoliak1997@gmail.com>
2024-07-09 23:40:16 +0200

    commit message

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,10 @@
 function getElementWidth(content, padding, border) {
-    const content = parseFloat(content);
-    const padding = parseFloat(padding) * 2;
-    const border = parseFloat(border) * 2;
-    return `content + padding + border` ;
-}
-
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+    const width = parseFloat(content);
+    const paddingWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
+    return width + paddingWidth + borderWidth;
+  }
+  
+  console.log(getElementWidth("50px", "8px", "4px")); // 74
+  console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+  console.log(getElementWidth("200px", "0px", "0px")); // 200
